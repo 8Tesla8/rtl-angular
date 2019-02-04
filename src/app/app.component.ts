d9609a388e332549b005200708a7a507c551f873
tesla <kela256@gmail.com>
2019-02-04 22:55:34 +0200

    added examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { TextDirectionController } from 'src/shared/TextDirectionController';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rtlAngular';
+
+  public directionController = new TextDirectionController();
+
+  constructor(
+    private renderer: Renderer2
+  ){ 
+    // set text direction to body 
+    // this.renderer.setAttribute(document.body, 'dir', this.directionController.textDirection);
+  }
 }
